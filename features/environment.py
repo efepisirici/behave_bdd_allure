2e8da97aa22f8d2cce6c4dbc2147e4e33308a770
pisirice <efe.pisirici.ep1@businesspartner.roche.com>
2024-09-23 15:57:15 +0300

    feat: :sparkles: Screenshots in each step

--- features/environment.py
+++ features/environment.py
@@ -1,32 +1,43 @@
+import allure
 from selenium import webdriver
-from behave.model_core import Status
-
-RETRY_COUNT = 2
-
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+import os
+import re
+from behave.model_core import Status
+
+def sanitize_filename(filename):
+    return re.sub(r'[\\/*?:"<>|]', "_", filename)
 
 def before_all(context):
     chrome_options = Options()
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
-    
+
     context.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+    context.driver.set_page_load_timeout(30)  # Add a timeout to avoid connection issues
     context.driver.maximize_window()
-    context.allure_report_dir = "features/reports"
+    context.allure_report_dir = os.path.join("features", "reports")
+    os.makedirs(context.allure_report_dir, exist_ok=True)
     print("Initialized WebDriver and other global settings before all tests.")
-    pass
+
+def after_step(context, step):
+    if step.status == Status.failed or step.status == Status.passed:
+        screenshot_name = sanitize_filename(f"{step.name}_{step.status.name.lower()}.png")
+        screenshot_path = os.path.join(context.allure_report_dir, screenshot_name)
+        try:
+            context.driver.save_screenshot(screenshot_path)
+            attach_screenshot_to_allure(screenshot_path)
+        except Exception as e:
+            print(f"Failed to capture screenshot: {e}")
 
 def after_all(context):
-    # Teardown code after all scenarios
-    context.driver.quit()
+    if context.driver:
+        context.driver.quit()
     print("Closed WebDriver and cleaned up after all tests.")
 
-def before_scenario(context, scenario):
-    pass
-
-def after_scenario(context, scenario):
-    pass
+def attach_screenshot_to_allure(filepath):
+    with open(filepath, 'rb') as image_file:
+        allure.attach(image_file.read(), name=os.path.basename(filepath), attachment_type=allure.attachment_type.PNG)
