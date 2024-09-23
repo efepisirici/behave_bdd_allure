ab60977131099be98d248223aa584e32a7da07bc
pisirice <efe.pisirici.ep1@businesspartner.roche.com>
2024-09-23 12:18:21 +0300

    fix: :memo: updated to run headless

--- features/environment.py
+++ features/environment.py
@@ -3,13 +3,19 @@ from behave.model_core import Status
 
 RETRY_COUNT = 2
 
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+
 def before_all(context):
-    # Setup code before all scenarios
-    options = webdriver.ChromeOptions()
-    # start browser maximized
-    options.add_argument("--start-maximized")
-    #options.add_argument("--headless")
-    context.driver = webdriver.Chrome(options=options)
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    
+    context.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+    context.driver.maximize_window()
     context.allure_report_dir = "features/reports"
     print("Initialized WebDriver and other global settings before all tests.")
     pass
